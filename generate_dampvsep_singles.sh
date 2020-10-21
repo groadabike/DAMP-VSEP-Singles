1fded5e1233b517a25fdb2bd33f9877b7c65adf7
gerardo <gerardo.roa@gmail.com>
2020-10-21 18:08:07 +0100

    Fix misspelling in file name

--- generate_dampvsep_singles.sh
+++ generate_dampvsep_singles.sh
@@ -3,12 +3,13 @@ set -eu  # Exit on error
 
 damvsep_root=$1
 output_dir=$2
+python_path=$3
 sample_rate=16000
 
 splits="test valid train_english train_singles"
 
 for split in $splits; do
-  python scripts/create_metadata.py --split $split \
+  $python_path scripts/create_metadata.py --split $split \
     --root_path $damvsep_root \
     --sample_rate $sample_rate \
     --output_meta_path $output_dir
